1da02ea6b5ff3018d8b9502986e18af47a827a6c
Gustavo Libraiz <gulibraiz@gmail.com>
2022-05-07 20:37:05 -0300

    Adicionando ID no output de notificação

--- server/schemas/notificacao_schema.py
+++ server/schemas/notificacao_schema.py
@@ -17,6 +17,7 @@ class NotificacaoInput(BaseModel):
 
 class NotificacaoOutput(BaseModel):
 
+    id: int = Field(example=1)
     guid: GUID = Field(example='44ddad94-94ee-4cdc-bce9-b5b126c9a714')
     guid_usuario: GUID = Field(example='a4ddad94-94ee-4cdc-bce9-b5b126c9a714')
     conteudo: Optional[str]
